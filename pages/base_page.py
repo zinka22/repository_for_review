2f6db9b69459452bc0ab8bcdf8945eb500632b9e
zinka22 <natasha-labchuk@mail.ru>
2024-07-07 20:10:24 +0300

    move selector to locals.py, rewrite main with new selectors

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    def __init__(self, browser, url,timeout=10):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
