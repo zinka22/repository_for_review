ef64957a741a1109b55ab0f63cb5666e10fb9178
zinka22 <natasha-labchuk@mail.ru>
2024-07-11 20:44:08 +0300

    add main page task

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,8 +4,10 @@ from pages.locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(
